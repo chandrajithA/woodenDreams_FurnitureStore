a615521018fbd890faae343e3ca3d9cd9b042c6a
Chandrajith <chandruofficial97@gmail.com>
2025-07-09 05:25:57 +0530

    initial change

---
+++ woodenDreams.js
@@ -0,0 +1,24 @@
+//to open and close the header navbar menu list
+function displayNavbar(){
+        document.getElementById("headerNavbar").classList.toggle("visible");   
+}
+
+
+//to close the header navbar menu list when clicked outside the button and navbar
+document.addEventListener("click", function(event) {
+  const headerNavbar = document.getElementById("headerNavbar");
+  const navbarShortIcon = document.getElementById("navbarShortIcon");
+
+  if ( !headerNavbar.contains(event.target) && !navbarShortIcon.contains(event.target) ) {
+    headerNavbar.classList.remove("visible");
+  }
+});
+
+
+//to get the category data list
+async function getCategoryData(){
+    let response = await fetch("woodenDreamsCategory.json");
+    let categoryData = await response.json();
+    console.log(categoryData);
+    
+} 

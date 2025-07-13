5f1aefa44237fb6f8c95eade19aeca788070e698
Chandrajith <chandruofficial97@gmail.com>
2025-07-13 19:44:49 +0530

    initial change

--- woodenDreams.js
+++ woodenDreams.js
@@ -1,6 +1,6 @@
 //to open and close the header navbar menu list
 function displayNavbar(){
-        document.getElementById("headerNavbar").classList.toggle("visible");   
+  document.getElementById("headerNavbar").classList.toggle("visible");   
 }
 
 
@@ -17,9 +17,84 @@ document.addEventListener("click", function(event) {
 
 //to get the category data list
 async function getCategoryData(){
-    let response = await fetch("woodenDreamsCategory.json");
-    let categoryData = await response.json();
-    return categoryData;
-} 
+  let response = await fetch("woodenDreamsCategory.json");
+  let categoryData = await response.json();
+  return categoryData;
+}
+
+
+//signin/signout btn fuctionality 
+function signinSignout(){
+  signinstatus = sessionStorage.getItem("wdissignin") || ""
+  if(signinstatus === "signin"){
+    signout();
+  }else if(signinstatus === ""){
+    window.location.href = "signin.html"
+  }
+}
+
+function signin(username){
+  sessionStorage.setItem("wdissignin","signin");
+  sessionStorage.setItem("wduserid",username);
+  alert("Signed In Successful");
+  previouspage()
+}
+
+function signout(){
+  let signoutconfirm = confirm("Are you Sure you want to Sign out")
+  if(signoutconfirm){
+    sessionStorage.removeItem("wdissignin");
+    sessionStorage.removeItem("wduserid");
+    updateloginstatus();
+    alert("Signed Out successfully");
+  }
+}
+
+function previouspage(){
+  window.history.back();
+}
+
+
+function updateloginstatus(){
+  let signinstatus = sessionStorage.getItem("wdissignin") || "";
+  if(signinstatus === "signin"){
+      document.getElementById("signInorSignUpbtn").textContent = "Sign Out";
+  }else if(signinstatus === ""){
+      document.getElementById("signInorSignUpbtn").textContent = "Sign In";
+  }
+}
+
+
+
+async function displaycategories(){
+  let displaycategoryarea = document.getElementById("displaycategoryarea");
+  displaycategoryarea.innerHTML = ""
+
+  let categories = await getCategoryData();
+  
+  categories.forEach(singlecategory => {
+      displaycategoryarea.innerHTML += 
+      `
+      <div class="displaysinglecategory" id="${singlecategory.category}">
+          <div class="categoryimage">
+              <img src="${singlecategory.imagepath}" alt="${singlecategory.category} image">
+          </div>
+          <div class="categoryname">
+              <span>${singlecategory.category}</span>
+          </div>
+      </div>
+      `
+  });
+}
+
+
+
+
+
+
+
+
+
+
 
 
